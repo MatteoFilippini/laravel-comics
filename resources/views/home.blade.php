2bf6e344355ed3fda514594bdd357421261ee0e7
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-15 17:40:52 +0100

    added menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,6 @@ $cards = config('comics');
             </div>
         </nav>
         <section id="jumbotron">
-            jumbotron
         </section>
     </header>
     <main>
@@ -48,11 +47,31 @@ $cards = config('comics');
                 @endforeach
             </div>
             <a href="#">LOAD MORE</a>
+        </div>
+        <div class="menu">
+
+            <div class="menu-card">
+                <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="" />
+                <h4>DIGITAL COMICS</h4>
+            </div>
+            <div class="menu-card">
+                <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="" />
+                <h4>DC MERCHANDISE</h4>
+            </div>
+            <div class="menu-card">
+                <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="" />
+                <h4>SUBSCRIPTION</h4>
+            </div>
+            <div class="menu-card">
+                <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="" />
+                <h4>COMICS SHOP LOCATOR</h4>
+            </div>
+            <div class="menu-card">
+                <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="" />
+                <h4>DC POWER VISA</h4>
+            </div>
 
         </div>
-        <nav>
-
-        </nav>
     </main>
     <footer>
         footer
