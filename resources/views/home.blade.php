b7955c1f7b373caf685e91d2628d8fcdf5c6916a
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-15 15:54:32 +0100

    created scss files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('../css/app.css') }}">
     <title>Comics</title>
 </head>
 
+
+
 <body>
-    homepage
+    <header>
+        <nav>
+            nav
+        </nav>
+        <section id="jumbotron">
+            jumbotron
+        </section>
+    </header>
+    <main>
+        <div class="container">
+
+            <div class="cards">
+                carte
+            </div>
+            <nav>
+                nav
+            </nav>
+        </div>
+    </main>
+    <footer>
+        footer
+    </footer>
 </body>
 
 </html>
